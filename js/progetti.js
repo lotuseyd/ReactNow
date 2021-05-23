c008b4b1d403335f89d4b4e0e5a995a7203b351c
Palarvind03 <technical.stu03@gmail.com>
2021-05-23 11:43:17 +0200

    second

--- js/progetti.js
+++ js/progetti.js
@@ -1,4 +1,32 @@
 var navToggleCont = 0
+var cardBtnText = "Guarda ora"
+
+let url = 'https://raw.githubusercontent.com/Palarvind03/ReactNow/main/json/progetti.json';
+
+fetch(url)
+.then(res => res.json())
+.then((out) => {
+    for (progetto of out) {
+        card = document.createElement("div")
+        card.setAttribute("class","card")
+
+        cardHeader = document.createElement("div")
+        cardBody.setAttribute("class","card-header")
+        cardHeader.innerText = progetto.titolo1
+
+        cardBody = document.createElement("div")
+        cardBody.setAttribute("class","card-body")
+
+        cardBtn =  document.createElement("a")
+        cardBtn.setAttribute("class","btn btn-dark")
+        cardBtn.innerText = cardBtnText
+
+        cardText = document.createElement("p")
+        cardText.setAttribute("class","card-text")
+        cardText.setAttribute()
+    }
+})
+.catch(err => { throw err });
 
 function navToggler(x) {
     var md = document.querySelector("#menuDiv")
@@ -44,3 +72,4 @@ function mainBtnOut(){
     btnOutline.style.left = "10px"
     arrow.style.animationName = "anime-arrow"
 }
+
